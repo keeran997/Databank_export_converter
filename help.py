54121d8d00d39cad6d497b1ea88ea672781716fa
Keeran Balachandran <em19830@aut.ac.nz>
2026-07-15 19:04:41 +1200

    Add info on merge function and reformat

--- help.py
+++ help.py
@@ -20,16 +20,23 @@ class HelpDialog(QDialog):
         It is currently only set up to read and convert the label-based file, as opposed to the raw-data-based file.
             
         To export the label-based file:
-             1. In REDCap, navigate to the "Data Exports, Reports, and Stats" application.
-             2. Under "My Reports & Exports", click the "Export data" button for the Physical Exam.
-             3. Select the "CSV/Microsoft Excel (labels)" option and click "Export Data".
-             4. Click the "EXCEL CSV Labels" icon with the arrow to download.
+                1. In REDCap, navigate to the "Data Exports, Reports, and Stats" application.
+                2. Under "My Reports & Exports", click the "Export data" button for the Physical Exam.
+                3. Select the "CSV/Microsoft Excel (labels)" option and click "Export Data".
+                4. Click the "EXCEL CSV Labels" icon with the arrow to download.
         
         How to use the converter:
-             1. Click "Load CSV File" to load your label-based REDCap .csv export file. Alternatively, drag and drop the .csv file into the converter window.
-             2. Click "Save Output As" to choose where to save your converted file.
-             3. Click "Run Conversion" to run the converter.
-             4. Converter should run successfully.
+                1. Click "Load CSV File" to load your label-based REDCap .csv export file. 
+                   Alternatively, drag and drop the .csv file into the converter window.
+                2. Click "Save Output As" to choose where to save your converted file.
+                3. Click "Run Conversion" to run the converter.
+        
+        The merge function is used when PostOp or PreOp data is to be added to an existing physical exam file.
+                1. Tick the "Merge into an existing Physical Exam" box.
+                2. Load the REDCap export file and the existing Physical Exam.
+                3. Click "Save Output As" to choose where to save the output file. 
+                   Note that this cannot have the same name as the existing file.
+                4. Click "Run Conversion" to run the converter.
              
         The converter is a work in progress.  
         """)
